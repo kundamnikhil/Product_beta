4531d493a70b0b805eb0736141cf4b3015615e78
Venkata Nikhil Kundam <kundamnikhil@gmail.com>
2023-10-17 12:35:46 -0500

    Basic App structure with firebase connection working

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Replace with your Firebase config
+const firebaseConfig = {
+    apiKey: "AIzaSyBHy9klyM9x08JL9D01c_anaO7Lg_8l_eY",
+    authDomain: "product-beta-d831a.firebaseapp.com",
+    projectId: "product-beta-d831a",
+    storageBucket: "gs://product-beta-d831a.appspot.com",
+    messagingSenderId: "707141456549",
+    appId: "product-beta-d831a.web.app"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
